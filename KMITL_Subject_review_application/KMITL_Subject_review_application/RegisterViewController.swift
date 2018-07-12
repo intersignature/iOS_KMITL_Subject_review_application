a04043dcfc11a8e283d7e69bb4708a87f74f2168
intersignature <intersignature@hotmail.com>
2018-07-13 00:44:29 +0700

    Add register view controller

---
+++ KMITL_Subject_review_application/KMITL_Subject_review_application/RegisterViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterViewController.swift
+//  KMITL_Subject_review_application
+//
+//  Created by Sirichai Binchai on 12/7/2561 BE.
+//  Copyright © 2561 Sirichai Binchai. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onBackBtn(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
